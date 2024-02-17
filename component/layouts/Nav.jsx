d13bb396bd05c56a4984eb39fb39d37892866766
Seulgigi <2021112414@dgu.ac.kr>
2024-02-17 16:22:05 +0900

    feat: nav to mainpage

--- component/layouts/Nav.jsx
+++ component/layouts/Nav.jsx
@@ -3,11 +3,14 @@ import * as S from "../_styled/layoutStyled"
 import logo_image from "../image/logo_navy.svg"
 import logo_text from "../image/logo_text_navy.svg"
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
 
 export default function Nav() {
+    const router = useRouter();
+
     return(
         <S.NavContainer>
-            <S.Logo>
+            <S.Logo onClick={()=>{router.push(`/`)}}>
                 <S.LogoImage src={logo_image} alt="logo_image" />
                 <S.LogoText src={logo_text} alt="logo_text" />
             </S.Logo>
