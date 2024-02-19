baa8788aeed7a5be5998b735a281f83a39b8a72b
Seulgigi <2021112414@dgu.ac.kr>
2024-02-19 23:51:30 +0900

    feat: main issue card component

--- component/_styled/mainStyled.jsx
+++ component/_styled/mainStyled.jsx
@@ -1,5 +1,38 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
     text-align: center;
+    display: flex;
+    flex-direction: column;
+`
+
+export const MainIssueBox = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    padding: 0 5%;
+`
+
+export const MainIssueBtn = styled.button`
+    width: 45%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 3% 0;
+    border: 1px solid #DEEBF3;
+    border-radius: 6px;
+    background-color: #F3F9FD;
+    color: #2A2A3A;
+    cursor: pointer;
+`
+
+export const MainIssueIcon = styled(FontAwesomeIcon)`
+    width: 20px;
+    margin-bottom: 5%;
+`
+
+export const MainIssueText = styled.span`
+    font-size: 16px;
+    font-weight: bold;
 `
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,19 @@
+import { faChartLine, faCircleExclamation } from "@fortawesome/free-solid-svg-icons"
 import * as S from "../component/_styled/mainStyled"
 
 export default function Main() {
   return (
     <S.MainContainer>
-      Main
+      <S.MainIssueBox>
+        <S.MainIssueBtn>
+          <S.MainIssueIcon icon={faCircleExclamation}/>
+          <S.MainIssueText>이머징 이슈란?</S.MainIssueText>
+        </S.MainIssueBtn>
+        <S.MainIssueBtn>
+          <S.MainIssueIcon icon={faChartLine}/>
+          <S.MainIssueText>실시간 이슈</S.MainIssueText>
+        </S.MainIssueBtn>
+      </S.MainIssueBox>
     </S.MainContainer>
   )
 }
