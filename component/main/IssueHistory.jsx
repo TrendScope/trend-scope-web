329d9cdf2ff89c779591e6de37b6a0d0e8aac0e9
Seulgigi <2021112414@dgu.ac.kr>
2024-02-22 20:39:52 +0900

    feat: make dummy

--- component/main/IssueHistory.jsx
+++ component/main/IssueHistory.jsx
@@ -1,56 +1,106 @@
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons"
+import { useEffect, useState } from "react"
 import * as S from "./Styled.jsx"
+import API from "../../pages/api.js"
 
 export default function IssueHistory() {
+  const [historys, setHistorys] = useState([]);
+  const fetchHistorys = async () => {
+    try {
+      // const response = await API.get(``);
+      // const data = response.data;
+
+      // dummy
+      const dummy = {
+        "isSuccess": true,
+        "code": "COMMON200",
+        "message": "성공입니다.",
+        "result": {
+            "historyList": [
+                {
+                    "historyId": 1,
+                    "resultSubject": "드론",
+                    "categoryList": [
+                        {
+                            "category": "과학 • 기술",
+                        },
+                        {
+                            "category": "안전",
+                        },
+                        {
+                            "category": "환경",
+                        },
+                        {
+                            "category": "주택 • 건축",
+                        }
+                    ],
+                  "avgGrowthRate": 45,
+                  "influenceFactor": 63 
+                },
+                {
+                    "historyId": 2,
+                    "resultSubject": "드론2",
+                    "categoryList": [
+                        {
+                            "category": "과학 • 기술",
+                        },
+                        {
+                            "category": "안전",
+                        },
+                        {
+                            "category": "환경",
+                        },
+                        {
+                            "category": "주택 • 건축",
+                        }
+                    ],
+                  "avgGrowthRate": 45,
+                  "influenceFactor": 63 
+                }
+            ]
+        }
+      }
+
+      setHistorys(dummy.result.historyList);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(()=>{
+    fetchHistorys();
+  }, [])
+  
+  useEffect(() => {
+    console.log(historys); // Log the updated state here
+  }, [historys, ]);
+
   return(
     <S.HistoryContainer>
       <S.HistoryCardSlide>
-        <S.HistoryCard>
-          <S.HistoryCardTop>
-            <S.HistoryCardText>
-                <S.HistoryCardDate>2024.02.20</S.HistoryCardDate>
-                <S.HistoryCardTheme>주제어: 어쩌구 기술</S.HistoryCardTheme>
-            </S.HistoryCardText>
-            <S.HistoryCardPercent
-                percent={75}
-            >
+        {historys.map((history, idx)=>(
+          <S.HistoryCard key={idx}>
+            <S.HistoryCardTop>
+              <S.HistoryCardText>
+                  <S.HistoryCardDate>2024.02.20</S.HistoryCardDate>
+                  <S.HistoryCardTheme>주제어: {history.resultSubject}</S.HistoryCardTheme>
+              </S.HistoryCardText>
+              <S.HistoryCardPercent
+                  percent={75}
+              >
                 <S.HistoryCardPercentInner>75%</S.HistoryCardPercentInner>
-            </S.HistoryCardPercent>
-          </S.HistoryCardTop>
-          <S.HistoryCardCategoryBox>
-            <S.SearchCheckedCategory>
-              안전
-            </S.SearchCheckedCategory>
-            <S.SearchCheckedCategory>
-              주택•건축
-            </S.SearchCheckedCategory>
-            <S.SearchCategoryMore>...</S.SearchCategoryMore>
-          </S.HistoryCardCategoryBox>
-        </S.HistoryCard>
-
-        <S.HistoryCard>
-          <S.HistoryCardTop>
-            <S.HistoryCardText>
-                <S.HistoryCardDate>2024.02.20</S.HistoryCardDate>
-                <S.HistoryCardTheme>주제어: 드론</S.HistoryCardTheme>
-            </S.HistoryCardText>
-            <S.HistoryCardPercent
-                percent={15}
-            >
-                <S.HistoryCardPercentInner>15%</S.HistoryCardPercentInner>
-            </S.HistoryCardPercent>
-          </S.HistoryCardTop>
-          <S.HistoryCardCategoryBox>
-            <S.SearchCheckedCategory>
-            주택•건축
-            </S.SearchCheckedCategory>
-            <S.SearchCheckedCategory>
-              주택•건축
-            </S.SearchCheckedCategory>
-            <S.SearchCategoryMore>...</S.SearchCategoryMore>
-          </S.HistoryCardCategoryBox>
-        </S.HistoryCard>
-
+              </S.HistoryCardPercent>
+            </S.HistoryCardTop>
+            <S.HistoryCardCategoryBox>
+              <S.SearchCheckedCategory>
+                {history.categoryList[0].category}
+              </S.SearchCheckedCategory>
+              <S.SearchCheckedCategory>
+                {history.categoryList[1].category}
+              </S.SearchCheckedCategory>
+              <S.SearchCategoryMore>...</S.SearchCategoryMore>
+            </S.HistoryCardCategoryBox>
+          </S.HistoryCard>
+        ))}
       </S.HistoryCardSlide>
     </S.HistoryContainer>
   )
